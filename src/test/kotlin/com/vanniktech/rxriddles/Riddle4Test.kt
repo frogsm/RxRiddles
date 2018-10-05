589d2e63b66f1b011c251ea40c1bf620f0443e78
frogsm <teddy_1008@daum.net>
2018-10-06 03:37:42 +0900

    Solved Riddle4

--- src/test/kotlin/com/vanniktech/rxriddles/Riddle4Test.kt
+++ src/test/kotlin/com/vanniktech/rxriddles/Riddle4Test.kt
@@ -6,20 +6,21 @@ import org.junit.Test
 
 /** Solution [Riddle4Solution] */
 class Riddle4Test {
-  @Test fun solve() {
-    val subject = PublishSubject.create<Unit>()
+    @Test
+    fun solve() {
+        val subject = PublishSubject.create<Unit>()
 
-    val o = Riddle4.solve(subject)
-        .test()
-        .assertValuesOnly(false)
+        val o = Riddle4.solve(subject)
+                .test()
+                .assertValuesOnly(false)
 
-    subject.onNext(Unit)
-    o.assertValuesOnly(false, true)
+        subject.onNext(Unit)
+        o.assertValuesOnly(false, true)
 
-    subject.onNext(Unit)
-    o.assertValuesOnly(false, true, false)
+        subject.onNext(Unit)
+        o.assertValuesOnly(false, true, false)
 
-    subject.onNext(Unit)
-    o.assertValuesOnly(false, true, false, true)
-  }
+        subject.onNext(Unit)
+        o.assertValuesOnly(false, true, false, true)
+    }
 }
