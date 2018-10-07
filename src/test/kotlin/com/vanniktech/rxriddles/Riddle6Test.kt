b144964de825489a83f096eebdcf034f0191f731
frogsm <teddy_1008@daum.net>
2018-10-07 10:08:22 +0900

    Solved Riddle6

--- src/test/kotlin/com/vanniktech/rxriddles/Riddle6Test.kt
+++ src/test/kotlin/com/vanniktech/rxriddles/Riddle6Test.kt
@@ -11,28 +11,30 @@ import java.util.concurrent.atomic.AtomicInteger
 
 /** Solution [Riddle6Solution] */
 class Riddle6Test {
-  @get:Rule val rxRule = RxRule()
+    @get:Rule
+    val rxRule = RxRule()
 
-  @Test fun solve() {
-    val subscribeCounter = AtomicInteger()
+    @Test
+    fun solve() {
+        val subscribeCounter = AtomicInteger()
 
-    val first = Single.timer(5, SECONDS, rxRule)
-        .map { 10 }
-        .doOnSubscribe { subscribeCounter.incrementAndGet() }
+        val first = Single.timer(5, SECONDS, rxRule)
+                .map { 10 }
+                .doOnSubscribe { subscribeCounter.incrementAndGet() }
 
-    val second = Single.just(5)
-        .doOnSubscribe { subscribeCounter.incrementAndGet() }
+        val second = Single.just(5)
+                .doOnSubscribe { subscribeCounter.incrementAndGet() }
 
-    val o = Riddle6.solve(first, second)
-        .test()
-        .assertEmpty()
+        val o = Riddle6.solve(first, second)
+                .test()
+                .assertEmpty()
 
-    assertThat(subscribeCounter.get()).isEqualTo(2) // We want to subscribe immediately.
+        assertThat(subscribeCounter.get()).isEqualTo(2) // We want to subscribe immediately.
 
-    rxRule.advanceTimeBy(4, SECONDS)
-    o.assertEmpty()
+        rxRule.advanceTimeBy(4, SECONDS)
+        o.assertEmpty()
 
-    rxRule.advanceTimeBy(1, SECONDS)
-    o.assertResult(10 to 5)
-  }
+        rxRule.advanceTimeBy(1, SECONDS)
+        o.assertResult(10 to 5)
+    }
 }
