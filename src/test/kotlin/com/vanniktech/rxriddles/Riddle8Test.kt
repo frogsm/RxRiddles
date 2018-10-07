5f489042582e92d019c1607aab43bc6dceece407
frogsm <teddy_1008@daum.net>
2018-10-07 13:15:12 +0900

    Solved Riddle8

--- src/test/kotlin/com/vanniktech/rxriddles/Riddle8Test.kt
+++ src/test/kotlin/com/vanniktech/rxriddles/Riddle8Test.kt
@@ -11,29 +11,31 @@ import java.util.concurrent.atomic.AtomicInteger
 
 /** Solution [Riddle8Solution] */
 class Riddle8Test {
-  @get:Rule val rxRule = RxRule()
+    @get:Rule
+    val rxRule = RxRule()
 
-  @Test fun solve() {
-    val counter = AtomicInteger()
+    @Test
+    fun solve() {
+        val counter = AtomicInteger()
 
-    val source = Observable.just(Unit)
-        .doOnSubscribe { counter.incrementAndGet() }
-        .doOnEach { counter.incrementAndGet() }
-        .doOnError { counter.incrementAndGet() }
-        .doOnComplete { counter.incrementAndGet() }
+        val source = Observable.just(Unit)
+                .doOnSubscribe { counter.incrementAndGet() }
+                .doOnEach { counter.incrementAndGet() }
+                .doOnError { counter.incrementAndGet() }
+                .doOnComplete { counter.incrementAndGet() }
 
-    val o = Riddle8.solve(source)
-        .test()
-        .assertEmpty()
+        val o = Riddle8.solve(source)
+                .test()
+                .assertEmpty()
 
-    assertThat(counter.get()).isEqualTo(0) // We don't want to do anything.
+        assertThat(counter.get()).isEqualTo(0) // We don't want to do anything.
 
-    rxRule.advanceTimeBy(100, MILLISECONDS)
-    o.assertEmpty()
-    assertThat(counter.get()).isEqualTo(0)
+        rxRule.advanceTimeBy(100, MILLISECONDS)
+        o.assertEmpty()
+        assertThat(counter.get()).isEqualTo(0)
 
-    rxRule.advanceTimeBy(200, MILLISECONDS)
-    o.assertResult(Unit)
-    assertThat(counter.get()).isEqualTo(4)
-  }
+        rxRule.advanceTimeBy(200, MILLISECONDS)
+        o.assertResult(Unit)
+        assertThat(counter.get()).isEqualTo(4)
+    }
 }
