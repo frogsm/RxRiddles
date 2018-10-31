b4a3915ca5166d32d55ee564aa2d47c778b01fca
frogsm <teddy_1008@daum.net>
2018-11-01 00:27:09 +0900

    Solved Riddle37

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle37.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle37.kt
@@ -1,14 +1,20 @@
 package com.vanniktech.rxriddles
 
 import io.reactivex.Observable
+import java.io.IOException
 
 object Riddle37 {
-  /**
-   * Return an Observable that emits 'false' when the [source] emits an IOException.
-   *
-   * Use case: You want to recover from an expected error and map them to a particular result.
-   */
-  fun solve(source: Observable<Boolean>): Observable<Boolean> {
-    TODO()
-  }
+    /**
+     * Return an Observable that emits 'false' when the [source] emits an IOException.
+     *
+     * Use case: You want to recover from an expected error and map them to a particular result.
+     */
+    fun solve(source: Observable<Boolean>): Observable<Boolean> {
+        return source.onErrorResumeNext { error: Throwable ->
+            when (error) {
+                is IOException -> Observable.just(false)
+                else -> Observable.error(error)
+            }
+        }
+    }
 }
